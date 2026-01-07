a5e7f36024dd3a8926923aa361410475a4adda0b
Rohit Madgundi <rohitmadgundi@gmail.com>
2026-01-07 21:08:08 +0530

    check

---
+++ src/main/java/com/sbecom/User.java
@@ -0,0 +1,4 @@
+package com.sbecom;
+
+public class User {
+}
---
+++ src/main/java/com/sbecom/UserController.java
@@ -0,0 +1,9 @@
+package com.sbecom;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+
+}

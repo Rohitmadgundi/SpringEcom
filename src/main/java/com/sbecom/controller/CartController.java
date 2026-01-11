4bcb34ccc1375782c6f68b822046d60640bbb20e
Rohit Madgundi <rohitmadgundi@gmail.com>
2026-01-11 13:12:08 +0530

    setup for cartitem

---
+++ src/main/java/com/sbecom/controller/CartController.java
@@ -0,0 +1,12 @@
+package com.sbecom.controller;
+
+import lombok.Data;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/cart")
+@Data
+public class CartController {
+
+}
